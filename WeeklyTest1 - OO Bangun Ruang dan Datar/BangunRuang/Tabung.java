806b76c22e9e431a8068bfb4a0833d3a7f6c2a04
Andi Dwi <61771890+andiads@users.noreply.github.com>
2020-03-05 13:49:31 +0700

    Create Tabung.java

---
+++ WeeklyTest1 - OO Bangun Ruang dan Datar/BangunRuang/Tabung.java
@@ -0,0 +1,37 @@
+package BangunRuang;
+
+public class Tabung extends BangunRuang{
+    private int tinggi, jarijari;
+    private double volume, luasPermukaan;
+    
+    public void setTinggi(int t){
+      this.tinggi=t;
+    }
+    public void setJari(int r){
+      this.jarijari=r;
+    }
+    public void setVolume()
+    {
+        volume =  Math.PI * Math.pow(jarijari, 2) * tinggi;
+    }
+    public void setLuasPermukaan()
+    {
+        luasPermukaan = 2 * Math.PI * jarijari * (jarijari + tinggi);
+    }
+    public int getJariJari()
+    {
+        return jarijari;
+    }
+    public int getTinggi()
+    {
+        return tinggi;
+    }
+    public double getVolume()
+    {
+        return volume;
+    }
+    public double getLuasPermukaan()
+    {
+        return luasPermukaan;
+    }
+}
