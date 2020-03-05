f7ebf40bee205da9ff726ae73b27887419029456
Andi Dwi <andiads06@gmail.com>
2020-03-05 13:40:36 +0700

    revise

--- WeeklyTest1 - OO Bangun Ruang dan Datar/BangunRuang/BangunRuang.java
+++ WeeklyTest1 - OO Bangun Ruang dan Datar/BangunRuang/BangunRuang.java
@@ -11,19 +11,16 @@ package BangunRuang;
  */
 public class BangunRuang {
     int panjang,lebar,tinggi,sisi;
-    int volumeb,volumek,volume;
+    int volumeb,volumek;
     void hitungVolume(){
         System.out.println("Hitung volume suatu bangun ruang");
     }
     public static void main(String[] args){
         Kubus kubus = new Kubus();
         Balok balok = new Balok();
-        Kerucut kerucut = new Kerucut();
-        
         
         kubus.hitungVolume(10, 11, 20);
         balok.hitungVolume(3, 4, 9);
-        kerucut.hitungVolume(4, 8, 5);
         
     }
 }
